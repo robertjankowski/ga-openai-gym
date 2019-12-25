8a0340e98b8741a3f1cbe289834088a25eaf5580
robertjankowski <robjanw11@wp.pl>
2019-12-25 19:04:51 +0100

    make carracing new model gif

--- scripts/tests/carracing/testing_model_carracing.py
+++ scripts/tests/carracing/testing_model_carracing.py
@@ -6,15 +6,15 @@ from nn.conv import ConvNet
 
 if __name__ == '__main__':
     env = gym.make('CarRacing-v0')
-    env.seed(42)
-    #env = gym.wrappers.Monitor(env, 'carracing_video', video_callable=lambda episode_id: True, force=True)
+    env.seed(112)
+    env = gym.wrappers.Monitor(env, 'carracing_video', video_callable=lambda episode_id: True, force=True)
 
     obs = env.reset()
     nn = ConvNet()
     nn.load("../../../models/carracing/12-24-2019_22-50_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
             ".4_PCROSSOVER_0.8_I=1902_SCORE=440.54054054053455.npy")
 
-    for _ in range(5000):
+    for _ in range(10000):
         env.render()
         obs = torch.from_numpy(np.flip(obs, axis=0).copy()).float()
         obs = obs.reshape((-1, 3, 96, 96))
