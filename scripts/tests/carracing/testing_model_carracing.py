ab804749aaccb741f9b77ed286355a084701f741
robertjankowski <robjanw11@wp.pl>
2019-12-25 18:30:28 +0100

    new carracing model :)

--- scripts/tests/carracing/testing_model_carracing.py
+++ scripts/tests/carracing/testing_model_carracing.py
@@ -6,15 +6,15 @@ from nn.conv import ConvNet
 
 if __name__ == '__main__':
     env = gym.make('CarRacing-v0')
-    env.seed(123)
+    env.seed(42)
     #env = gym.wrappers.Monitor(env, 'carracing_video', video_callable=lambda episode_id: True, force=True)
 
     obs = env.reset()
     nn = ConvNet()
-    nn.load("../../../models/carracing/12-17-2019_12-14_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
-            ".4_PCROSSOVER_0.8_I=1495_SCORE=426.3157894736778.npy")
+    nn.load("../../../models/carracing/12-24-2019_22-50_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
+            ".4_PCROSSOVER_0.8_I=1902_SCORE=440.54054054053455.npy")
 
-    for _ in range(500):
+    for _ in range(5000):
         env.render()
         obs = torch.from_numpy(np.flip(obs, axis=0).copy()).float()
         obs = obs.reshape((-1, 3, 96, 96))
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         if done:
             break
 
-    torch.save(nn, "12-17-2019_12-14_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
-            ".4_PCROSSOVER_0.8_I=1495_SCORE=426.3157894736778.pt")
+    #torch.save(nn, "12-17-2019_12-14_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
+    #        ".4_PCROSSOVER_0.8_I=1495_SCORE=426.3157894736778.pt")
 
     env.close()
