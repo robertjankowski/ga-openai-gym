dbb30f2ba83e7b5f659d9223c0aee1ee953be921
robertjankowski <robjanw11@wp.pl>
2019-12-26 20:50:48 +0100

    finished carracing and fitness function

--- scripts/tests/carracing/testing_model_carracing.py
+++ scripts/tests/carracing/testing_model_carracing.py
@@ -6,13 +6,13 @@ from nn.conv import ConvNet
 
 if __name__ == '__main__':
     env = gym.make('CarRacing-v0')
-    env.seed(112)
-    env = gym.wrappers.Monitor(env, 'carracing_video', video_callable=lambda episode_id: True, force=True)
+    env.seed(109)
+    # env = gym.wrappers.Monitor(env, 'carracing_video', video_callable=lambda episode_id: True, force=True)
 
     obs = env.reset()
     nn = ConvNet()
-    nn.load("../../../models/carracing/12-24-2019_22-50_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
-            ".4_PCROSSOVER_0.8_I=1902_SCORE=440.54054054053455.npy")
+    nn.load("../../../models/carracing/12-26-2019_15-27_NN=ConvNetTorchIndividal_POPSIZE=50_GEN=2000_PMUTATION_0"
+            ".4_PCROSSOVER_0.8_I=1995_SCORE=466.6666666666616.npy")
 
     for _ in range(10000):
         env.render()
